7ab6650cc865c482a243d53087207f949d57ae39
Brendon Jennett <64869447+vxp101@users.noreply.github.com>
2024-12-19 15:28:18 -0500

    Add files via upload

--- popup.js
+++ popup.js
@@ -3,6 +3,13 @@ function injectTheScript() {
     chrome.tabs.query({active: true, currentWindow: true}, tabs => {
         chrome.scripting.executeScript({target: {tabId: tabs[0].id}, files: ['content_script.js']})
     })
+
+    chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+        function(tabs){
+           document.querySelector("#currentwebpage").innerHTML = "Webpage: " + tabs[0].url
+        }
+     );
 }
 
 document.getElementById('clickactivity').addEventListener('click', injectTheScript)
+
