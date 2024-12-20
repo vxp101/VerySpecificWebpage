da57f6176364818c723dd4998a731aa077641679
Brendon Jennett <64869447+vxp101@users.noreply.github.com>
2024-12-20 00:51:33 -0500

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,14 +1,23 @@
+var done = false
+
 function injectTheScript() {
     // Wuery the active tab, which will be only one tab and inject the script in it.
     chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-        chrome.scripting.executeScript({target: {tabId: tabs[0].id}, files: ['content_script.js']})
+         chrome.scripting.executeScript({target: {tabId: tabs[0].id}, files: ['content_script.js']})
+         if(tabs[0].url.split("/")[2].split(".")[0] == "linkshortx"){
+            done = true
+        }
+        
     })
 
-    chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
-        function(tabs){
+    chrome.tabs.query({active: true, 'windowId': chrome.windows.WINDOW_ID_CURRENT}, tabs => {
            document.querySelector("#currentwebpage").innerHTML = "Webpage: " + tabs[0].url
-        }
-     );
+    });
+
+    if(!done){
+        setTimeout(injectTheScript, 1000)
+    }
+    
 }
 
 document.getElementById('clickactivity').addEventListener('click', injectTheScript)
